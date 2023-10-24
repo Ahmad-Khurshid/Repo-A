c50585b75451a0a5a34e6a2f47bc056cde73e30b
Ahmad <ahmadking531049@gmail.com>
2023-10-24 14:29:55 +0500

    --------------

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,17 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        public delegate int  MathOperation<T>(int num1, int num2);
+        public static int Add(int num1,int num2)
+        {
+            return num1 + num2;
+        } 
+         
         static void Main(string[] args)
         {
-        }
+            MathOperation<int> obj = Add;
+            Console.WriteLine(obj(2,3));
+            Console.ReadLine();
+        } 
     }
 }
